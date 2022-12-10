c86235892941dab462d6939f7bde6e84ac5d08ba
Talabér Gergely <talafeco@gmail.com>
2022-12-10 18:44:36 +0100

    add new files
    add new files

---
+++ gaia_dr3_astro_param.py
@@ -0,0 +1,12 @@
+# Search for double star data in Gaia DR3 Astrophysical parameters
+
+# Import csv file of Gaia DR3 to a numpy array
+
+
+# Read file contains double stars to a numpy array
+
+
+# Check if any of the double stars are in the Gaia DR3 file based on the Source ID, collect findings into an array
+
+
+# Write findings into a csv file
---
+++ gaiadr3_doubles.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+filelist="$PWD/gaiafiles.txt"
+logfile="$PWD/gaiadr3_doubles_$(date +"%m-%d-%Y").log"
+
+for file in $(cat $filelist);
+do
+now=$(date)
+echo "# $now Start downloading $file." | tee -a "$logfile"
+wget https://cdn.gea.esac.esa.int/Gaia/gdr3/gaia_source/"$file" | tee -a "$logfile"
+
+now=$(date)
+echo "# $now Download finished, start extracting $file." | tee -a "$logfile"
+gzip -dk "$PWD/$file"
+
+now=$(date)
+echo "# $now Extraction finished, start normalizing file content (${file:0:28})." | tee -a "$logfile"
+awk '!/^#/' "$PWD/${file:0:28}" > "$PWD/tmp_${file:0:28}"
+python3 gaia_list_filter.py "$PWD/tmp_${file:0:28}" | tee -a "$logfile"
+mv "$PWD/result.csv" "$PWD/final_${file:0:28}"
+
+#now=$(date)
+#echo "# $now Normalization finished, start searching for double stars." | tee -a "$logfile"
+#python3 search_doubles_gaiadr3_v2.py "$PWD/final_${file:0:28}" > "$PWD/doubles_${file:0:28}" | tee -a "$logfile"
+
+now=$(date)
+echo "# $now Identification of double stars finished, results can be found in file doubles_"${file:0:28}". Start removing temporary files." | tee -a "$logfile"
+rm "$PWD/$file" "$PWD/${file:0:28}" "$PWD/tmp_${file:0:28}"
+
+now=$(date)
+echo "# $now Temporary files removed." | tee -a "$logfile"
+done
