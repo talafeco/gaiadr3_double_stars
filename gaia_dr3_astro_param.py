43c1ef2219f50658f7a25e3e108f4008cecf2eb9
Talabér Gergely <talafeco@gmail.com>
2022-12-10 19:25:09 +0100

    a
    a

--- gaia_dr3_astro_param.py
+++ gaia_dr3_astro_param.py
@@ -7,10 +7,12 @@ import numpy as np
 # Import csv file of Gaia DR3 to a numpy array
 gaiaFileName = open(sys.argv[1], 'r')
 gaiaStars = np.genfromtxt(gaiaFileName, delimiter=",")
+print('Gaia Stars: ', gaiaStars)
 
 # Read file contains double stars to a numpy array
-doubleStarFileName = open(sys.argv[1], 'r')
+doubleStarFileName = open(sys.argv[2], 'r')
 doubleStars = np.genfromtxt(doubleStarFileName, delimiter=",")
+print('Double Stars: ', doubleStars)
 
 # Check if any of the double stars are in the Gaia DR3 file based on the Source ID, collect findings into an array
 for ds in doubleStars:
