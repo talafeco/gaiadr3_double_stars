06f8b1982446ea526781b666bbe663e35013ef40
Talabér Gergely <talafeco@gmail.com>
2022-12-10 21:53:51 +0100

    Astro Param update
    Astro Param update

--- gaia_dr3_astro_param.py
+++ gaia_dr3_astro_param.py
@@ -1,5 +1,5 @@
-# Search for double star data in Gaia DR3 Astrophysical parameters
-# Importing modules
+#Search for double star data in Gaia DR3 Astrophysical parameters
+#Importing modules
 import csv
 import sys
 import numpy as np
@@ -8,15 +8,18 @@ import numpy as np
 gaiaFileName = open(sys.argv[1], 'r')
 gaiaStars = np.genfromtxt(gaiaFileName, delimiter=",")
 print('Gaia Stars: ', gaiaStars)
+print(gaiaStars.dtype)
 
 # Read file contains double stars to a numpy array
 doubleStarFileName = open(sys.argv[2], 'r')
 doubleStars = np.genfromtxt(doubleStarFileName, delimiter=",")
 print('Double Stars: ', doubleStars)
+print(doubleStars.dtype)
 
 # Check if any of the double stars are in the Gaia DR3 file based on the Source ID, collect findings into an array
-for ds in doubleStars:
-    doubleStar = np.where(gaiaStars = ds)
-    print(doubleStar)
 
+for ds in doubleStars:
+    #print(ds)
+    doubleStar = np.where(gaiaStars == ds)
+    print(doubleStar)
 # Write findings into a csv file
