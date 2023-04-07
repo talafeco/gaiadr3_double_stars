f0a0f0b966ed534f21724e15d193ada1ad01ffb2
talafeco <talafeco@gmail.com>
2023-04-07 21:40:31 +0200

    .
    .

--- wdsreport.py
+++ wdsreport.py
@@ -130,8 +130,8 @@ for fitsFile in files:
     sources.add_column(ra2, name='ra_deg') 
     sources.add_column(dec2, name='dec_deg')
     
-    print(sources.info)
-    print(sources)
+    #print(sources.info)
+    #print(sources)
     
     wds_catalog = SkyCoord(ra=wdsTable['ra_deg']*u.degree, dec=wdsTable['dec_deg']*u.degree)
     sources_catalog = SkyCoord(ra=sources['ra_deg']*u.degree, dec=sources['dec_deg']*u.degree)
@@ -139,15 +139,15 @@ for fitsFile in files:
     composit_catalog = hstack([wdsTable[idxw]['wds_identifier', 'discovr', 'comp', 'theta', 'rho'], sources[idxs]['id', 'mag', 'ra_deg', 'dec_deg']])
     companion_catalog = SkyCoord(ra=composit_catalog['ra_deg'] * u.degree, dec=composit_catalog['dec_deg'] * u.degree).directional_offset_by(composit_catalog['theta'] * u.degree, composit_catalog['rho'] * u.arcsec)
     
-    print('Companion catalog\n', companion_catalog)
-    print('Composit catalog\n', composit_catalog)
+    #print('Companion catalog\n', companion_catalog)
+    #print('Composit catalog\n', composit_catalog)
     idxs2, d2ds2, d3ds2 = match_coordinates_sky(companion_catalog, sources_catalog)
-    print(idxs2)
-    print('Companion catalog idxs2\n', sources[idxs2])
+    #print('Companion catalog idxs2\n', sources[idxs2])
+    #print(idxs2)
 
     composit_catalog2 = hstack([composit_catalog, sources[idxs2]]) #['id', 'mag', 'ra_deg', 'dec_deg']
-    print(composit_catalog2.info)
-    print('Composit catalog 2.\n', composit_catalog2)
+    # print(composit_catalog2.info)
+    #print('Composit catalog 2.\n', composit_catalog2)
 
     sources_pa = SkyCoord(ra=composit_catalog2['ra_deg_1']*u.degree, dec=composit_catalog2['dec_deg_1']*u.degree).position_angle(SkyCoord(ra=composit_catalog2['ra_deg_2']*u.degree, dec=composit_catalog2['dec_deg_2']*u.degree)).to(u.deg)
 
@@ -157,10 +157,10 @@ for fitsFile in files:
     composit_catalog2.add_column(sources_sep, name='rho_measured')
 
 
-    print('Matching sources list')
-    print(sources[idxs])
-    print(wdsTable[idxw])
-    print('Composit catalog 2. extended\n', composit_catalog2)
+    #print('Matching sources list')
+    #print(sources[idxs])
+    #print(wdsTable[idxw])
+    #print('Composit catalog 2. extended\n', composit_catalog2)
 
     sources_ds = vstack([sources_ds, composit_catalog2])
 
@@ -169,7 +169,7 @@ for fitsFile in files:
         idx, d2d, d3d = match_coordinates_sky(mainstar, wdscatalog)
         catalogstar = SkyCoord(ra=wdsTable[idx]['ra_deg']*u.degree, dec=wdsTable[idx]['dec_deg']*u.degree)
         sep = catalogstar.separation(mainstar)
-        if sep < Angle(0.001 * u.deg):
+        if sep < Angle(0.002 * u.deg):
             companion = mainstar.directional_offset_by(wdsTable[idx][3] * u.degree, (wdsTable[idx][4] / 3600) * u.deg)
             separation = mainstar.separation(companion)
             for star2 in sources:
@@ -177,16 +177,16 @@ for fitsFile in files:
                 compstar = SkyCoord(ra=ra3*u.degree, dec=dec3*u.degree)  
                 sep2 = compstar.separation(companion)
                 if sep2 <= Angle(0.002 * u.deg) and star != star2:
-                    print('\nWDS Identifier:', wdsTable[idx][0], wdsTable[idx][1], wdsTable[idx][2])
-                    print('Main star:', wdsTable[idx][0], wdsTable[idx][1], wdsTable[idx][3], wdsTable[idx][4])
-                    print('Main star data:\n', star)
-                    print('Separation:', Angle(sep))
-                    print('Comapnion star data:\n', star2)
+                    #print('\nWDS Identifier:', wdsTable[idx][0], wdsTable[idx][1], wdsTable[idx][2])
+                    #print('Main star:', wdsTable[idx][0], wdsTable[idx][1], wdsTable[idx][3], wdsTable[idx][4])
+                    #print('Main star data:\n', star)
+                    #print('Separation:', Angle(sep))
+                    #print('Comapnion star data:\n', star2)
                     paactual = mainstar.position_angle(compstar).to(u.deg)
                     sepactual = mainstar.separation(compstar) * 3600
-                    print('\nPA:', paactual.degree, 'Sep:', (sepactual.degree))
+                    #print('\nPA:', paactual.degree, 'Sep:', (sepactual.degree))
                     objectid = str(wdsTable[idx][0]) + '_' + str(wdsTable[idx][1]) + '_' + str(wdsTable[idx][2])
-                    wdsmag_diff =  floaf(wdsTable[idx]['mag_sec']) - float(wdsTable[idx]['mag_pri']
+                    wdsmag_diff =  float(wdsTable[idx]['mag_sec']) - float(wdsTable[idx]['mag_pri'])
                     magdiff = star2['mag'] - star['mag']
                     dsTable.add_row([wdsTable[idx][0], wdsTable[idx][1], wdsTable[idx][2], wdsTable[idx][3], wdsTable[idx][4], wdsTable[idx][5], wdsTable[idx][6], wdsmag_diff, wdsTable[idx][7], wdsTable[idx][8], wdsTable[idx][9], wdsTable[idx][10], wdsTable[idx][11], str(wdsTable[idx][12]), str(wdsTable[idx][13]), str(wdsTable[idx][14]), str(wdsTable[idx][15]), objectid, paactual.degree, sepactual.degree, magdiff])
 
@@ -194,68 +194,101 @@ print('### Sources DS ###')
 print(sources_ds)
 
 upd_sources_ds = sources_ds[sources_ds['rho_measured'] != 0]
+#upd_sources_ds.add_column(str(sources_ds['wds_identifier']) + '_' + str(sources_ds['discovr']) + '_' + str(sources_ds['comp']), name='object_id')
+upd_sources_ds_by_object = upd_sources_ds.group_by(['wds_identifier', 'discovr', 'comp'])
+
+# Create object ID array
+# upd_sources_ds_object_id = str(sources_ds['wds_identifier']) + '_' + str(sources_ds['discovr']) + '_' + str(sources_ds['comp'])
+#upd_sources_ds_object_id = np.empty(0,dtype=str)
+
+#for line in sources_ds:
+#    upd_sources_ds_object_id_instance = str(line['wds_identifier']) + '_' + str(line['discovr']) + '_' + str(line['comp'])
+#    np.char.replace(upd_sources_ds_object_id_instance,' ','_')
+#    print(upd_sources_ds_object_id_instance)
+#    np.append(upd_sources_ds_object_id, upd_sources_ds_object_id_instance)
+
+#print('### upd_sources_ds_object_id ###')
+#print(upd_sources_ds_object_id)
+# Add object ID array to upd_sources_ds table
+# upd_sources_ds.add_column(upd_sources_ds_object_id, name='object_id')
+
 print('### Updated sources DS ###')
 print(upd_sources_ds)
+print('### Updated sources DS table info ###')
+print(upd_sources_ds.info)
+print('### Updated sources DS table grouped by WDS Identifier, Discoverer and Components ###')
+print(upd_sources_ds_by_object)
+
 #Ide kell egy source_id generáló algoritmus!!!
 
 print('\n### Double stars ###')
-print(dsTable)            
+print(dsTable)
+print('\n### Double stars table info ###')
+print(dsTable.info)       
         
 ### Search double stars on the image sequence
 dsTable_by_object = dsTable.group_by('object_id')
-print('\n### Report Table by object ###')
-print(dsTable_by_object)
-print(dsTable.info)
+#print('\n### Report Table by object ###')
+#print(dsTable_by_object)
 
-objectMean = dsTable_by_object.groups.aggregate(np.mean)
-print(objectMean)
+
+objectMean = upd_sources_ds_by_object.groups.aggregate(np.mean)
+#print(objectMean)
 
 count = 1
-for ds in dsTable_by_object.groups:
-    print('\n### Group index:', count, '###')
+for ds in upd_sources_ds_by_object.groups:
+    #print('\n### Group index:', count, '###')
     #print(ds)
     count = count + 1
-    pairMeanTheta = ds['dspaactual'].groups.aggregate(np.mean)
-    pairMeanThetaErr = ds['dspaactual'].groups.aggregate(np.std)
-    pairMeanRho = ds['dssepactual'].groups.aggregate(np.mean)
-    pairMeanRhoErr = ds['dssepactual'].groups.aggregate(np.std)
-    pairMagnitudeA = ds[0]['mag_pri']
-    pairMagnitudeB = ds[0]['mag_sec']
-    pairMagDiff = ds['dsmagdiff'].groups.aggregate(np.mean)
-    pairMagDiffErr = ds['dsmagdiff'].groups.aggregate(np.std)
-    pairMagDiffWDS = ds[0]['theta'] - ds[0]['rho']
-    reportName = (workingDirectory + '/' + ds[0]['object_id'] + '.txt')
+    pairObjectId = ds[0]['wds_identifier'] + ds[0]['discovr'] + ds[0]['comp']
+    print(ds[0]['wds_identifier'] + ds[0]['discovr'] + ds[0]['comp'])
+    print('Theta: ', ds['theta_measured'].degree)
+    print('Rho: ', ds['rho_measured'].arcsec)
+    print('Mag A: ', ds[0]['mag_1'])
+    print('Mag B: ', ds[0]['mag_2'])
+    print('Mag Diff.: ', ds[0]['mag_2'] - ds[0]['mag_1'])
+
+    pairMeanTheta = ds['theta_measured'].degree.mean()
+    pairMeanThetaErr = ds['theta_measured'].degree.std()
+    pairMeanRho = ds['rho_measured'].arcsec.mean()
+    pairMeanRhoErr = ds['rho_measured'].arcsec.std()
+    pairMagnitudeA = ds[0]['mag_1']
+    pairMagnitudeB = ds[0]['mag_2']
+    pairMagDiff = (ds[0]['mag_2'] - ds[0]['mag_1']).mean()
+    pairMagDiffErr = (ds[0]['mag_2'] - ds[0]['mag_1']).mean()
+    pairMagDiffWDS = float(ds[0]['theta']) - ds[0]['rho']
+    reportName = (workingDirectory + '/' + pairObjectId + '.txt')
     reportFile = open(reportName, "a")
     print('### COMPONENTS ###')
     print('\nWDS Identifier:', ds[0]['wds_identifier'], ds[0]['discovr'], ds[0]['comp'])
-    print('\nTheta measurements\n', ds['dspaactual'])
-    print('Mean:', pairMeanTheta[0])
-    print('Error:', pairMeanThetaErr[0])
-    print('\nRho measurements\n', ds['dssepactual'])
-    print('Mean:', pairMeanRho[0])
-    print('Error:', pairMeanRhoErr[0])
-    print('\nMagnitude measurements\n', ds['dsmagdiff'])
-    print('Mean:', pairMagDiff[0])
-    print('Error:', pairMagDiffErr[0])
-    reportTable.add_row([ds[0]['wds_identifier'], 'Date of observation', pairMeanTheta[0], pairMeanThetaErr[0], pairMeanRho[0], pairMeanRhoErr[0], np.nan, np.nan, pairMagDiff[0], pairMagDiffErr[0], 'Filter wawelenght', 'filter FWHM', '0.2', '1', 'TAL_2022', 'C', '7'])
+    print('\nTheta measurements\n') # , ds['dspaactual']
+    print('Mean:', pairMeanTheta)
+    print('Error:', pairMeanThetaErr)
+    print('\nRho measurements\n') # , ds['dssepactual']
+    print('Mean:', pairMeanRho)
+    print('Error:', pairMeanRhoErr)
+    print('\nMagnitude measurements\n') # , ds['dsmagdiff']
+    print('Mean:', pairMagDiff)
+    print('Error:', pairMagDiffErr)
+    reportTable.add_row([ds[0]['wds_identifier'], 'Date of observation', pairMeanTheta, pairMeanThetaErr, pairMeanRho, pairMeanRhoErr, np.nan, np.nan, pairMagDiff, pairMagDiffErr, 'Filter wawelenght', 'filter FWHM', '0.2', '1', 'TAL_2022', 'C', '7'])
     reportFile.write('\n\nWDS Identifier: ' + ds[0]['wds_identifier'])
     reportFile.write('\nDiscoverer and components: ' + str(ds[0]['discovr']) + ' ' + str(ds[0]['comp']))
-    reportFile.write('\nMagnitude(s) (Pri / Sec): ' + str(ds[0]['mag_pri']) + ' / ' +  str(ds[0]['mag_sec']))
+    reportFile.write('\nMagnitude(s) (Pri / Sec): ' + str(ds[0]['mag_1']) + ' / ' +  str(ds[0]['mag_2']))
     reportFile.write('\nPA, Sep: ' + str(ds[0]['theta']) + ' / ' +  str(ds[0]['rho']))
-    reportFile.write('\nPosition angle: \n' + str(ds['dspaactual']))
-    reportFile.write('\nMean: ' + str(pairMeanTheta[0]))
-    reportFile.write('\nError: ' + str(pairMeanThetaErr[0]))
-    reportFile.write('\nSeparation: \n' + str(ds['dssepactual']))
-    reportFile.write('\nMean: ' + str(pairMeanRho[0]))
-    reportFile.write('\nError: ' + str(pairMeanRhoErr[0]))
-    reportFile.write('\nMagnitude measurements\n' + str(ds['dsmagdiff']))
-    reportFile.write('\nMean: ' + str(pairMagDiff[0]))
-    reportFile.write('\nError: ' + str(pairMagDiffErr[0]))
+    reportFile.write('\nPosition angle: \n')
+    reportFile.write('\nMean: ' + str(pairMeanTheta))
+    reportFile.write('\nError: ' + str(pairMeanThetaErr))
+    reportFile.write('\nSeparation: \n')
+    reportFile.write('\nMean: ' + str(pairMeanRho))
+    reportFile.write('\nError: ' + str(pairMeanRhoErr))
+    reportFile.write('\nMagnitude measurements\n')
+    reportFile.write('\nMean: ' + str(pairMagDiff))
+    reportFile.write('\nError: ' + str(pairMagDiffErr))
     reportFile.write('\n\n### WDS form:\n')
-    wdsform = str(ds[0]['wds_identifier']) + ',' + 'Date of observation' + ',' +  str(pairMeanTheta[0]) + ',' +  str(pairMeanThetaErr[0]) + ',' +  str(pairMeanRho[0]) + ',' +  str(pairMeanRhoErr[0]) + ',' +  'nan' + ',' +  'nan' + ',' +  str(pairMagDiff[0]) + ',' +  str(pairMagDiffErr[0]) + ',' + 'Filter wawelenght' + ',' + 'filter FWHM' + ',' + '0.2' + ',' + '1' + ',' + 'TAL_2022' + ',' +  'C' + ',' + '7'
-    print(str(wdsform))
+    wdsform = str(ds[0]['wds_identifier']) + ',' + 'Date of observation' + ',' +  str(pairMeanTheta) + ',' +  str(pairMeanThetaErr) + ',' +  str(pairMeanRho) + ',' +  str(pairMeanRhoErr) + ',' +  'nan' + ',' +  'nan' + ',' +  str(pairMagDiff) + ',' +  str(pairMagDiffErr) + ',' + 'Filter wawelenght' + ',' + 'filter FWHM' + ',' + '0.2' + ',' + '1' + ',' + 'TAL_2022' + ',' +  'C' + ',' + '7'
+    #print(str(wdsform))
     reportFile.write(str(wdsform))
     
-print(reportTable)
+#print(reportTable)
 dsTable_by_object.write('double_stars.txt', format='ascii', overwrite=True, delimiter=',')
 reportTable.write('double_stars_wds_format.txt', format='ascii', overwrite=True, delimiter=',')
