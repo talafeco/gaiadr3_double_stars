fb912d59291a5c16bd4aa9abc7b3c944da1b44dc
talafeco <80475659+talafeco@users.noreply.github.com>
2023-09-05 20:00:56 +0200

    .

--- deg_to_hms.py
+++ deg_to_hms.py
@@ -1,11 +1,13 @@
-from astropy.coordinates import SkyCoord
 from astropy import units as u
 import sys
-from astropy.coordinates import SkyCoord
+from astropy.coordinates import Angle
 
 # Get coordinates
 star_ra = sys.argv[1].replace(",",".")
+star_ra = Angle(star_ra, u.degree)
 star_dec = sys.argv[2].replace(",",".")
+star_dec = Angle(star_dec, u.degree)
 
-print(star_ra + ' ' + star_dec)
-print(SkyCoord(star_ra, star_dec, unit='hour, dms'))
+# print(star_ra + ' ' + star_dec)
+# print(SkyCoord(star_ra, star_dec, unit='hour, deg'))
+print(star_ra.to_string(unit=u.hour) + ' ' + star_dec.to_string(unit=u.degree))
