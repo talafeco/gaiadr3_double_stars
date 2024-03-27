39c7c05961deb344919c561324acadd5923090ef
talafeco <talafeco@gmail.com>
2024-03-27 20:02:42 +0100

    .

--- reduction.py
+++ reduction.py
@@ -25,33 +25,6 @@ raw_image = fits.open(image_file)[0].data
 image_data = np.array(raw_image, dtype=np.float64)
 print(np.info(image_data))
 
-## Define a function for making a linear gray scale
-def lingray(image_data, image_min=None, image_max=None):
-    """
-    Auxiliary function that specifies the linear gray scale.
-    a and b are the cutoffs : if not specified, min and max are used
-    """
-    if image_min == None:
-        image_min = np.min(image_data)
-    if image_max == None:
-        image_max = np.max(image_data)
-    return 255.0 * (image_data - float(image_min)) / (image_max - image_min)
-
-## Define a function for making a logarithmic gray scale
-def loggray(x, a=None, b=None):
-    """
-    Auxiliary function that specifies the logarithmic gray scale.
-    a and b are the cutoffs : if not specified, min and max are used
-    """
-    if a == None:
-        a = np.min(x)
-    if b == None:
-        b = np.max(x)          
-    linval = 10.0 + 990.0 * (x-float(a))/(b-a)
-    return (np.log10(linval)-1.0) * 0.5 * 255.0
-
-#grayscaled_image_data = lingray(image_data)
-
 ## Build marster dark
 dark_dir = os.listdir(dark_lib)
 dark_files = [f for f in dark_dir if os.path.isfile(dark_lib+'/'+f) and (f.endswith('.fit') or f.endswith('.fits'))]
@@ -92,24 +65,23 @@ dark_corrected_image = image_data - master_dark
 ## Remove bias
 bias_corrected_image = dark_corrected_image - master_bias
 
+## Remove negative valuse from the image
+bias_corrected_image[bias_corrected_image<0] = 0
+
 ## Correct flat
 flat_corrected_image = bias_corrected_image / master_flat
 
 ## Create final image by correctiong the grayscale
 final_image = flat_corrected_image
-#final_image = lingray(flat_corrected_image)
-testitng_image = final_image
-testing_name = 'final_image'
 new_image_min = 0.
-new_image_max = np.max(testitng_image)
+new_image_max = np.max(final_image)
+gray_scaling = 65535 / new_image_max
+corrected_image = gray_scaling * final_image
 
 ## Troubleshooting
-plt.imshow(testitng_image, aspect='equal', vmax=new_image_max, vmin=new_image_min) # ,  , , , , , origin='lower',  cmap='Greys'
-plt.savefig(testing_name + '.jpg',dpi=150.0, bbox_inches='tight', pad_inches=0.2)
+plt.imshow(corrected_image, aspect='equal', vmax=np.max(corrected_image), vmin=new_image_min, cmap='Greys_r') # ,origin='lower', 
+plt.savefig('corrected_image.jpg',dpi=150.0, bbox_inches='tight', pad_inches=0.2)
 plt.show()
-print(image_data)
-#print(grayscaled_image_data)
-print(master_dark)
-print(master_bias)
-print(master_flat)
-print(final_image)
+print(corrected_image)
+print(np.min(corrected_image))
+print(np.max(corrected_image))
